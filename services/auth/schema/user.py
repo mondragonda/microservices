ab7b820a014ee00e351ec11b45d895efe322725e
Diego Mondragon <mondragdiego@gmail.com>
2023-05-29 12:36:47 -0700

    Added Pydantic db modeling for strawberry

--- services/auth/schema/user.py
+++ services/auth/schema/user.py
@@ -1,52 +1,38 @@
 import strawberry
-import datetime
-import bson
 from ..resolver.mutations.user import Mutation
 from typing import List
-from strawberry.dataloader import DataLoader
 import strawberry
 from ..database.database import db
+from ..database.models.user import User as UserModel
+from ...strawberryconf import PyObjectIdType
 
 
+@strawberry.experimental.pydantic.type(model=UserModel)
 @strawberry.federation.type(keys=["_id"], description="User register information.")
 class User:
-    _id: bson.ObjectId
-    email: str
-    password: strawberry.Private[str]
-    photo_url: str
-    first_name: str
-    last_name: str
-    date_of_birth: datetime.date
-    address: str
-
-    def __init__(self, _id: bson.ObjectId, email: str, photo_url: str, first_name: str, last_name: str, date_of_birth: datetime.date, address: str):
-        self._id = _id
-        self.email = email
-        self.photo_url = photo_url
-        self.first_name = first_name
-        self.last_name = last_name
-        self.date_of_birth = date_of_birth
-        self.address = address
+    id: PyObjectIdType
+    email: strawberry.auto
+    # password: strawberry.Private[strawberry.auto]
+    photo_url: strawberry.auto
+    first_name: strawberry.auto
+    last_name: strawberry.auto
+    date_of_birth: strawberry.auto
+    address: strawberry.auto
 
 
 async def load_users() -> List[User]:
-    return [User(
-        _id=user._id,
-        email=user.email,
-        photo_url=user.photo_url,
-        first_name=user.first_name,
-        last_name=user.last_name,
-        date_of_birth=user.date_of_birth,
-        address=user.address
-    ) async for user in db.users.find()]
+    users_cursor = db.users.find()
+    users = await users_cursor.to_list(length=100)
+    if users:
+        return [User.from_pydantic(UserModel.parse_obj(user)) for user in users]
+    else:
+        return []
 
-
-# loader = DataLoader(load_fn=load_users)
 
 @strawberry.type
 class Query:
     @strawberry.field
-    async def users(self, _id: str) -> List[User]:
+    async def users(self) -> List[User]:
         return await load_users()
 
 
---
+++ services/strawberryconf.py
@@ -0,0 +1,7 @@
+import strawberry
+from database import PyObjectId
+
+# serialize PyObjectId as a scalar type
+PyObjectIdType = strawberry.scalar(
+    PyObjectId, serialize=str, parse_value=lambda v: PyObjectId(v)
+)
