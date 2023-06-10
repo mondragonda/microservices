18f18373af1d61c11ea4cf6dd28bd3ef119f04c0
Diego Mondragon <diegoarmando.mondr@hcl.com>
2023-06-10 00:11:48 +0000

    Added addAdvisor mutation

--- services/financial/schema/advisor.py
+++ services/financial/schema/advisor.py
@@ -1,18 +1,33 @@
 import strawberry
 from typing import List
+
+from services.database import PyObjectId
 from ...auth.schema.user import User
+from ...auth.database.models.user import User as UserModel
 from ...auth.database.database import db as auth_db
 from ..database.database import db
 from ..database.models.advisor import Advisor as AdvisorModel
 from ...strawberryconf import PyObjectIdType
+from ..database.database import db
 
 
-@strawberry.experimental.pydantic.type(model=AdvisorModel)
-@strawberry.federation.type(keys=["id"], description="Financial advisor information.")
+# WORKAROUND TO FIX LATER
+async def get_user(root: "Advisor") -> "User":
+    return User.from_pydantic(UserModel.parse_obj(
+        await auth_db.users.find_one({"_id": root.user_id})  # type: ignore
+    ))
+
+
+@strawberry.experimental.pydantic.type(
+    model=AdvisorModel,
+    description="Financial advisor information.",
+    all_fields=False
+)
 class Advisor:
     id: PyObjectIdType
-    user: User
-    contact_phone_number: str
+    contact_phone_number: strawberry.auto
+    user_id: PyObjectIdType
+    user: User = strawberry.field(resolver=get_user)
 
 
 async def load_financial_advisors() -> List[Advisor]:
@@ -31,7 +46,46 @@ class Query:
         return await load_financial_advisors()
 
 
+@strawberry.type
+class UserForAdvisorNotExistsError:
+    user: str
+
+
+@strawberry.type
+class AdvisorAlreadyExistsError:
+    user: str
+
+
+MutationResponse = strawberry.union(
+    "AddAdvisorResponse", [UserForAdvisorNotExistsError,
+                           AdvisorAlreadyExistsError, Advisor]
+)
+
+
+@strawberry.type
+class Mutation:
+    @strawberry.mutation
+    async def add_advisor(self, user: str, contact_phone_number: str) -> MutationResponse:  # type: ignore
+        advisor_obj = {"user": PyObjectId(
+            user), "contact_phone_number": contact_phone_number}
+        valid_advisor = AdvisorModel.validate(advisor_obj)
+        if valid_advisor:
+            user_exists = await auth_db.users.find_one({"_id": advisor_obj["user"]})
+            if not user_exists:
+                return UserForAdvisorNotExistsError(
+                    user=user
+                )
+            existing_advisor_for_user = await db.advisors.find_one({"user": advisor_obj["user"]})
+            if existing_advisor_for_user:
+                return AdvisorAlreadyExistsError(user=user)
+            created_advisor_result = await db.advisors.insert_one(advisor_obj)
+            if created_advisor_result:
+                created_advisor_record = await db.advisors.find_one({"_id": PyObjectId(created_advisor_result.inserted_id)})
+                return Advisor.from_pydantic(AdvisorModel.parse_obj(created_advisor_record))
+
+
 schema = strawberry.federation.Schema(
     Query,
+    Mutation,
     enable_federation_2=True,
 )
