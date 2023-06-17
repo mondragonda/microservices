9eb1294b5df1a0f17b6505ab50ce68e102b80e2d
Diego Mondragon <mondragdiego@gmail.com>
2023-06-16 17:35:41 -0700

    Added send account verification link process

--- services/auth/authorization.py
+++ services/auth/authorization.py
@@ -1,15 +1,19 @@
+import os
+import redis
+from os import getenv
+from jose import jwt
+from secrets import token_urlsafe
 from pydantic import BaseModel, Field
 from passlib.context import CryptContext
 from datetime import datetime, timedelta
-from os import getenv
-from jose import jwt
 from .database.models.user import User as UserModel
 from .database.database import db, client
 from fastapi import HTTPException, status
 from fastapi.responses import JSONResponse
 from ..database import PyObjectId
 from fastapi.encoders import jsonable_encoder
-from secrets import token_urlsafe
+from typing import Any
+from .email_verification import email_verification_service
 
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
@@ -24,14 +28,23 @@ class Token(BaseModel):
 
 class Authorization:
 
+    def __init__(self):
+        self.redis = redis.Redis(
+            host=os.getenv("REDIS_HOSTNAME", default=""),
+            port=int(os.getenv("REDIS_PORT", default="0")),
+            username=os.getenv("REDIS_USERNAME", default=""),
+            password=os.getenv("REDIS_PASSWORD", default=""),
+
+        )
+
     def verify_password(self, plain_password: str, hashed_password: str):
         return pwd_context.verify(plain_password, hashed_password)
 
+    def get_url_safe_token(self):
+        return token_urlsafe(64)
+
     def get_password_hash(self, password: str):
         return pwd_context.hash(password)
-
-    def get_url_safe_verification_token(self):
-        return token_urlsafe(64)
 
     def create_access_token(self, data: dict, expires_delta: timedelta | None = None):
         to_encode = data.copy()
@@ -64,7 +77,7 @@ class Authorization:
                 headers={"WWW-Authenticate": "Bearer"},
             )
         access_token_expires = timedelta(
-            minutes=int(getenv("ACCESS_TOKEN_EXPIRE_MINUTES")))  # type: ignore
+            minutes=int(getenv("ACCESS_TOKEN_EXPIRE_MINUTES", default="0")))
         access_token = self.create_access_token(
             data={"sub": user.email}, expires_delta=access_token_expires
         )
@@ -75,6 +88,20 @@ class Authorization:
             "access_token": access_token,
         }
 
+    def generate_account_verification(self, user, verification_token, verification_token_expiration_time):
+        def send_verification(pipe):
+            verification_token_cache_set = self.redis.set(name=user.id.__str__(
+            ), value=verification_token, ex=verification_token_expiration_time)
+            if verification_token_cache_set:
+                verification_generate_email_send_ok = email_verification_service.send_verification_email(
+                    user, verification_token)
+                if verification_generate_email_send_ok == False:
+                    raise BaseException("Failed to send verification email.")
+            else:
+                raise BaseException(
+                    "Failed to save account verification token on cache.")
+        self.redis.transaction(send_verification)
+
     async def email_password_register(self, user: UserModel):
         valid_user = UserModel.validate(user)
         if valid_user:
@@ -83,7 +110,7 @@ class Authorization:
                 return JSONResponse(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, content=jsonable_encoder({"detail": [
                     {"loc": ["body", "email"], "msg": "User already exists for email."}], "body": {"email": saved_user_for_email["email"]}}))
             async with await client.start_session() as session:
-                created_token_result = None
+                created_user_result = None
                 async with session.start_transaction():
                     created_user_result = await db.users.insert_one({
                         "_id": valid_user.id,
@@ -96,15 +123,20 @@ class Authorization:
                         "address": valid_user.address,
                         "_verified": False
                     }, session=session)
-                user_account_verification_token = self.get_url_safe_verification_token()
-                # Persist token on redis indexed by user _id
-                async with session.start_transaction():
-                    if created_user_result:
+                if created_user_result.inserted_id:
+                    async with session.start_transaction():
                         token = await self.login_for_access_token(valid_user)
-                        created_token_result = db.tokens.insert_one(
+                        created_token_result = await db.tokens.insert_one(
                             token, session=session)
-                        if created_token_result:
+                        if created_token_result.inserted_id:
+                            user_account_verification_token = self.get_url_safe_token()
+                            token_expiration_time = int(os.getenv(
+                                "ACCOUNT_REGISTER_VERIFICATION_HASH_EXPIRE_SECS", default="0"))
+                            self.generate_account_verification(
+                                valid_user, user_account_verification_token, token_expiration_time)
                             return Token.parse_obj(token)
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                            detail='Registration process failed due to an internal error.')
 
 
 authorization_service = Authorization()
---
+++ services/auth/email_verification.py
@@ -0,0 +1,25 @@
+import os
+from .database.models.user import User
+import requests
+
+
+class EmailVerificationService:
+
+    def send_verification_email(self, account_user: User, generated_verification_token: str):
+        verification_link = f'<a href="https://example.com/verify?token={generated_verification_token}">https://example.com/verify?token={generated_verification_token}</a>'
+        message = {}
+        message['from'] = f'NWM Financial Advising <{os.getenv("MAILGUN_EMAIL_USER")}>'
+        message['to'] = f'{account_user.first_name} {account_user.last_name} <{account_user.email}>'
+        message['subject'] = 'NWM Financial Advising - Account Verification'
+        message["html"] = f"<html><p>Click on the link below to verify your account:</p><p>{verification_link}</p></html>"
+
+        response = requests.post(os.getenv("MAILGUN_API_URL", default=""), auth=(
+            "api", os.getenv("MAILGUN_API_KEY", default="")), data=message)
+
+        return response.ok
+
+    def verify_account(self, email, token):
+        pass
+
+
+email_verification_service = EmailVerificationService()
