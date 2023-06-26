c87cec2bc3b7849d139d305d694837196a2605fc
Diego Mondragon <mondragdiego@gmail.com>
2023-06-26 13:55:14 -0700

    proposed fix #https://github.com/tiangolo/fastapi/issues/1663

--- services/auth/main.py
+++ services/auth/main.py
@@ -15,15 +15,25 @@ from .middleware import email_password_login, email_password_register_path, user
 
 app = FastAPI()
 
+# https://github.com/tiangolo/fastapi/issues/1663
+
 
 @app.middleware("http")
-async def cors_handler(request: Request, call_next):
-    response: Response = await call_next(request)
-    response.headers['Access-Control-Allow-Credentials'] = 'true'
-    response.headers['Access-Control-Allow-Origin'] = '*'
-    response.headers['Access-Control-Allow-Methods'] = '*'
-    response.headers['Access-Control-Allow-Headers'] = '*'
+async def add_cors_headers(request, call_next):
+    response = await call_next(request)
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    response.headers["Access-Control-Allow-Credentials"] = "true"
+    response.headers["Access-Control-Allow-Methods"] = "*"
+    response.headers["Access-Control-Allow-Headers"] = "*"
     return response
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.middleware('http')
@@ -65,5 +75,6 @@ async def account_verification_email(request: Request):
 
 graphql_app = GraphQLRouter(schema, path=graphql_api_path,
                             graphiql=bool(int(getenv("NWM_DEBUG_MODE", default="1"))))
+
 
 app.include_router(graphql_app)
--- services/financial/main.py
+++ services/financial/main.py
@@ -11,15 +11,25 @@ app = FastAPI()
 #     "https://studio.apollographql.com",
 # ]
 
+# https://github.com/tiangolo/fastapi/issues/1663
+
 
 @app.middleware("http")
-async def cors_handler(request: Request, call_next):
-    response: Response = await call_next(request)
-    response.headers['Access-Control-Allow-Credentials'] = 'true'
-    response.headers['Access-Control-Allow-Origin'] = '*'
-    response.headers['Access-Control-Allow-Methods'] = '*'
-    response.headers['Access-Control-Allow-Headers'] = '*'
+async def add_cors_headers(request, call_next):
+    response = await call_next(request)
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    response.headers["Access-Control-Allow-Credentials"] = "true"
+    response.headers["Access-Control-Allow-Methods"] = "*"
+    response.headers["Access-Control-Allow-Headers"] = "*"
     return response
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.middleware('http')
