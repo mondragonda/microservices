0e9e359a5c3d8c4683ebc9199814da59bb15e477
Diego Mondragon <mondragdiego@gmail.com>
2023-06-26 14:01:09 -0700

    cors config

--- services/auth/main.py
+++ services/auth/main.py
@@ -21,15 +21,20 @@ app = FastAPI()
 @app.middleware("http")
 async def add_cors_headers(request, call_next):
     response = await call_next(request)
-    response.headers["Access-Control-Allow-Origin"] = "*"
+    response.headers["Access-Control-Allow-Origin"] = "https://silly-stop-sandbox.up.railway.app/, http://localhost:3000"
     response.headers["Access-Control-Allow-Credentials"] = "true"
     response.headers["Access-Control-Allow-Methods"] = "*"
     response.headers["Access-Control-Allow-Headers"] = "*"
     return response
 
+origins = [
+    'https://silly-stop-sandbox.up.railway.app/',
+    'http://localhost:3000'
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=['*'],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
